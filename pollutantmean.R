308050f077225c82cce73a67365988a9f86fd455
bsandy <bsandy@yahoo.com>
2014-11-19 20:15:44 -0600

    a

--- pollutantmean.R
+++ pollutantmean.R
@@ -16,6 +16,7 @@ pollutantmean <- function(directory, pollutant, id = 1:332) {
   
   filelist <- list.files(directory,full.names=TRUE)
   
-  for (i in id) dataset<-rbind(dataset, read.csv(filelist[i]))
+  for (i in id) 
+    dataset<-rbind(dataset, read.csv(filelist[i]))
   mean(dataset[,pollutant],na.rm=TRUE)
 } 
